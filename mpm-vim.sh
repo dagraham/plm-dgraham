697dbabb2a4d88d55e0a81a748e00140d8d81bb6
Daniel Graham <dnlgrhm@gmail.com>
2022-09-08 09:40:30 -0400

    renamed as plm - player lineup manager

--- mpm-vim.sh
+++
@@ -1,3 +0,0 @@
-gvim -R note:VimNotes note:GitNotes 2>/dev/null
-gvim -g -p create-event.py request-dates.py create-schedule.py send-schedules.py README.md tennis-vim.sh 2>/dev/null
-
---
+++ plm-vim.sh
@@ -0,0 +1,3 @@
+gvim -R note:VimNotes note:GitNotes 2>/dev/null
+gvim -g -p create-event.py request-dates.py create-schedule.py send-schedules.py README.txt plm-vim.sh 2>/dev/null
+
