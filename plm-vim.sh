e7810acc6deec106b5b2d368648909d3b39e0f8f
Daniel Graham <dnlgrhm@gmail.com>
2022-09-13 15:31:34 -0400

    adding setup.py pre packaging

--- plm-vim.sh
+++ plm-vim.sh
@@ -1,3 +1,3 @@
 gvim -R note:VimNotes note:GitNotes 2>/dev/null
-gvim -g -p create-project.py make-schedule.py README.md plm-vim.sh 2>/dev/null
+gvim -g -p create-project.py make-schedule.py setup.py install_missing.sh README.md plm-vim.sh 2>/dev/null
 
